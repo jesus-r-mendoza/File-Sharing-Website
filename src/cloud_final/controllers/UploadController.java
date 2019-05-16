90c08e2383c3637fc40ca34e8e9c95c9b2ccd186
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-15 21:44:11 -0700

    added base upload controller

---
+++ src/cloud_final/controllers/UploadController.java
@@ -0,0 +1,22 @@
+package cloud_final.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Upload")
+public class UploadController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/cloud_final/views/CloudView.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
