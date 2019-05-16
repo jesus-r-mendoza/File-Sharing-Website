6bf50c37da32ef783c203ea5f4d770dfb6ce4c9d
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-15 21:58:48 -0700

    uploading files works

--- src/cloud_final/controllers/UploadController.java
+++ src/cloud_final/controllers/UploadController.java
@@ -1,11 +1,14 @@
 package cloud_final.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import cloud_final.models.FileUploader;
 
 @WebServlet("/Upload")
 public class UploadController extends HttpServlet {
@@ -16,6 +19,15 @@ public class UploadController extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		File repository = (File) this.getServletConfig().getServletContext().getAttribute("javax.servlet.context.tempdir");
+		String uploadDirectory = this.getServletContext().getRealPath("/WEB-INF/uploads");		
+		
+		FileUploader uploader = new FileUploader(repository, uploadDirectory);		
+		int filesUploaded = uploader.parseRequest(request);
+		
+		System.out.println("Uploaded " + filesUploaded + " file(s) to: " + uploadDirectory);
+		
 		doGet(request, response);
 	}
 
