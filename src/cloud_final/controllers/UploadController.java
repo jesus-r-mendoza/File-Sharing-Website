63abbc4966c9dd6e3756963436a249ac8e2710aa
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-17 21:58:49 -0700

    removed unused imports

--- src/cloud_final/controllers/UploadController.java
+++ src/cloud_final/controllers/UploadController.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import cloud_final.models.FileBean;
 import cloud_final.models.FileUploader;
 import cloud_final.models.User;
 
