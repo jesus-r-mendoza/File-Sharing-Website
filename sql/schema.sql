ec7962bfa8d6ec9d99168584d22473d4d69dcc7f
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-17 23:02:18 -0700

    removed files from albert account

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,7 +15,3 @@ CREATE TABLE files (
 );
 
 INSERT INTO users (user_id, username, password) VALUES (1, "albert", "fc0376370bc38f9d1914231447340542252c9dc4");
-
-INSERT INTO files (file_name, upload_date, user_id) VALUES ("test.txt", CURDATE(), 1);
-INSERT INTO files (file_name, upload_date, user_id) VALUES ("example.pdf", CURDATE(), 1);
-INSERT INTO files (file_name, upload_date, user_id) VALUES ("sample.jpg", CURDATE(), 1);
