87ee078efa7a213b79055e50a97d9185274c3046
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-05-17 22:01:30 -0700

    added starter download servlet

---
+++ src/cloud_final/controllers/DownloadController.java
@@ -0,0 +1,22 @@
+package cloud_final.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/download")
+public class DownloadController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
